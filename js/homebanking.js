e6999138a464b09fe32de0be4fec12cc71b3f6ea
KevCB1988 <kevcb1988@gmail.com>
2019-09-15 10:38:22 -0500

    Proyecto finalizado, listo para revisión

--- js/homebanking.js
+++ js/homebanking.js
@@ -1,18 +1,16 @@
+
 //Declaración de variables
 
-var nombreUsuario = "Keven Chaparro";
-var saldoCuenta = 200;
+var nombreUsuario = prompt("Ingresa tu usuario");
+var codigo_cuenta = parseInt(prompt("Ingresa tu código de cuenta"));
+var pass = 1234;
+var saldoCuenta = 2000;
 var limiteExtraccion = 5000;
-var agua = 350;
-var telefono = 425;
-var luz = 210;
-var internet = 570;
+
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
-    cargarNombreEnPantalla();
-    actualizarSaldoEnPantalla();
-    actualizarLimiteEnPantalla();
+    iniciarSesion();
 }
 
 //Funciones creadas por mi
@@ -83,6 +81,11 @@ function depositarDinero() {
 }
 
 function pagarServicio(){
+    var agua = 350;
+    var luz = 210;
+    var telefono = 425;
+    var internet = 570;
+
     var servicioSeleccionado = parseInt(
         prompt(
             `Digita el número del servicio que deseas pagar: \n
@@ -114,8 +117,7 @@ function pagarServicio(){
         if(servicioSeleccionado > saldoCuenta){
             alert("ALERTA:\nNo tienes suficiente dinero, debes depositar más dinero en tu cuenta. Vuelve a intentarlo.")            
         }else{
-            alert(`Haz r`);
-            console.log(saldoCuenta);
+            alert(`¡TRANSACCIÓN EXITOSA!\nHaz realizado el pago del servicio por un valor de $${servicioSeleccionado}. Vuelve pronto.`);
             saldoCuenta -= servicioSeleccionado;
             actualizarSaldoEnPantalla();
         }
@@ -125,11 +127,55 @@ function pagarServicio(){
 
             
 function transferirDinero() {
+    var cuenta_amiga_1 = 1234567;
+    var cuenta_amiga_2 = 7654321;
+
+    var cantidad_dinero_transferir = parseInt(prompt(
+        "\nTRANSFERENCIA DE DINERO:\nIngresa la cantidad de dinero que deseas transferir.\nLuego selecciona tu cuenta amiga.\n"
+    ));
+
+    if(cantidad_dinero_transferir > saldoCuenta){
+        alert(
+            "\nADVERTENCIA:\nNo tienes suficiente dinero en tu cuenta. Deposita y vuelve a intentarlo."  
+        )
+    }else{
+        var cuenta_amiga = parseInt(prompt(
+            "\nTU CUENTA AMIGA:\nIngresa el número de cuenta a la cual deseas realizar la transferencia.\n"
+        ));
+
+        if(cuenta_amiga === cuenta_amiga_1 || cuenta_amiga === cuenta_amiga_2){
+            transferencia_exitosa();
+        }else{
+            alert("\nADVERTENCIA:\nCuenta amiga no encontrada. Vuelve a intentarlo."  );
+        }
+    }
+    
+    function transferencia_exitosa(){
+        alert(
+            `TRANSFERENCIA EXITOSA:\nHaz transferido $${cantidad_dinero_transferir} a la cuenta N°${cuenta_amiga}`
+        );
+        saldoCuenta -= cantidad_dinero_transferir;
+        actualizarSaldoEnPantalla();
+    }
 
 }
 
+
 function iniciarSesion() {
 
+    if(codigo_cuenta == pass){
+        alert(
+            `\nBienvenido(a) Sr(a). ${nombreUsuario.toUpperCase()}.\nEmpieza a disfrutar de nuestros servicios.\n`
+        );
+        cargarNombreEnPantalla();
+        actualizarSaldoEnPantalla();
+        actualizarLimiteEnPantalla();
+    }else{
+        alert(
+            `\n¡ADVERTENCIA!:\nCódigo de cuenta no valido, tu dinero esta protegido. Vuelve a intentarlo.\n`
+        )
+        location.reload();
+    }
 }
 
 //Funciones que actualizan el valor de las variables en el HTML
